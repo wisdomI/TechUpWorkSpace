4c05fb3ecf80151a356585bcf4016fd6b8b5c2c7
unknown <lazycode686@gmail.com>
2023-03-15 10:30:58 -0700

    from lazycode

---
+++ js/index.js
@@ -0,0 +1,30 @@
+
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+  loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>{
+  loginForm.classList.remove('active');
+}
+
+
+
+window.onload = () =>{
+
+  if(window.scrollY > 80){
+    document.querySelector('.header .header-2').classList.add('active');
+  }else{
+    document.querySelector('.header .header-2').classList.remove('active');
+  }
+
+  // fadeOut();
+  
+}
+  
+
+function fadeOut(){
+    setTimeout(loader, 4000);
+}
+  
