fb99805059cec3542af2fa34bc6164ed004c9b5d
wisdomi <wisdomiwunwa@gmail.com>
2023-03-27 02:07:44 +0100

    body implementation

--- js/index.js
+++ js/index.js
@@ -1,40 +1,90 @@
 
-let loginForm = document.querySelector('.login-form-container');
 
-document.querySelector('#login-btn').onclick = () =>{
+
+// READ MORE AND SHOW LESS FUNCTIONALITIES STARTS HERE _________
+let moreText = document.querySelector(".more-txt")
+let showNone = document.querySelector(".show-less")
+let readMore = document.querySelector(".read-more")
+
+document.querySelector(".read-more").onclick = () => {
+  moreText.classList.add("show")
+  showNone.classList.add("show")
+  readMore.classList.add("hidden")
+}
+
+document.querySelector(".show-less").onclick = () => {
+  moreText.classList.remove("show")
+  showNone.classList.remove("show")
+  readMore.classList.remove("hidden")
+}
+
+// READ MORE AND SHOW LESS FUNCTIONALITIES ENDS HERE _________
+
+
+// Desktop log-in and register prompts
+let loginForm = document.querySelector('.login-form-container');
+let closeForm = document.querySelector('#close-login-btn')
+
+let regForm = document.querySelector('.reg-form-container');
+let closeregForm = document.querySelector('#close-reg-btn');
+
+document.querySelector('.log-in').onclick = () => {
   loginForm.classList.toggle('active');
 }
 
-document.querySelector('#close-login-btn').onclick = () =>{
+document.querySelector('.hero-btn').onclick = () => {
+  loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () => {
   loginForm.classList.remove('active');
 }
 
-
-window.onload = () =>{
-
-  if(window.scrollY > 80){
-    document.querySelector('.header .header-2').classList.add('active');
-  }else{
-    document.querySelector('.header .header-2').classList.remove('active');
-  }
-
-  // fadeOut();
-  
-}
-  
-function fadeOut(){
-    setTimeout(loader, 4000);
+document.querySelector('.register').onclick = () => {
+  regForm.classList.toggle('active-reg');
 }
 
-// ==== Animate on Scroll Initialize  ==== //
-AOS.init();
+document.querySelector('#close-reg-btn').onclick = () => {
+  regForm.classList.remove('active-reg');
+}
+// End Desktop log-in and register prompts
+
+// Mobile log-in and register button propmts
+let mobileLogin = document.querySelector('.mobile-cta')
+
+document.querySelector('.profile-picture-mobile').onclick = () => {
+  mobileLogin.classList.toggle('hidden')
+}
+
+document.querySelector('.log-in-m').onclick = () => {
+  loginForm.classList.toggle('active');
+}
+
+document.querySelector('.register-m').onclick = () => {
+  regForm.classList.toggle('active-reg');
+}
 
 
-// ==== GSAP Animations ==== //
-// ==== text  ==== //
-gsap.from(".test", {
-  opacity: 0,
-  y: -10,
-  delay: 1,
-  duration: 0.5,
-});
+
+// SWIPER 
+
+const swiper = new Swiper('.swiper', {
+
+  autoplayDisableOnInteraction: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+}, 3000);
